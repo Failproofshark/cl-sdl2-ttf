579bb3c629189259bb9375ebda48790abc754f4f
Bryan Baraoidan <bryan.baraoidan@gmail.com>
2015-06-19 23:14:35 -0700

    Calls to rendering calls proved to be problematic will need to write those wrappers from scratch

--- src/general.lisp
+++ src/general.lisp
@@ -21,8 +21,8 @@
 (defun open-font (path-to-font point-size)
   "Open a font specified by the path specifier path-to-font sized to integer point-size (based on 72DPI). Returns a ttf-font struct and null on errors"
   (autocollect (ptr)
-               (check-null (ttf-open-font (namestring path-to-font) point-size))
-               (ttf-close-font ptr)))
+      (check-null (ttf-open-font (namestring path-to-font) point-size))
+    (ttf-close-font ptr)))
 
 (defun close-font (ttf-font-struct)
   "Frees the memory used by the ttf-font-struct"
@@ -31,7 +31,12 @@
 ;;TODO have a unified rendering function for solid, shaded, and blended and have which mode as a simple argument
 ;;TODO add support for utf-8 and other encoding
 (defun render-text-solid (font text color)
-  "Create an sdl-surface with the text rendered in the font and color passed in."
-  (autocollect (ptr)
-               (check-null (render-text-solid font text color))
-               (sdl2:free-surface ptr)))
+  "Create an sdl-surface with the text rendered in the font and a list of integers representing the col passed in."
+  (cffi:with-foreign-pointer (sdl-color 4)
+    (loop for i from 0 upto (- (length color) 1) do
+         (setf (cffi:mem-aref sdl-color :uint8 i) (nth i color)))
+    (autocollect (ptr)
+        (check-null (ttf-render-text-solid font
+                                           text
+                                           sdl-color))
+      (sdl2:free-surface ptr))))
