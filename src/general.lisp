232cbe4abc560120fa46259e2f565ca632af3075
Pavel Kulyov <pkulev@croc.ru>
2019-05-14 20:29:00 +0300

    Add weakref for loaded fonts
    This change allows to call (sdl2-ttf:quit) safely.
    
    Signed-off-by: Pavel Kulyov <pkulev@croc.ru>

--- src/general.lisp
+++ src/general.lisp
@@ -2,6 +2,8 @@
 
 (require 'sdl2)
 
+(defvar *fonts* (list) "List of weak refs to fonts.")
+
 (defun init ()
   "Initialize the sdl trutype font API. Does not require a call to sdl-init prior to calling this function. Returns 0 if succesful -1 otherwise"
   (check-rc (ttf-init)))
@@ -16,16 +18,26 @@
   (ttf-was-init))
 
 (defun quit ()
+  (dolist (pointer *fonts*)
+    (let ((ttf-font-struct (tg:weak-pointer-value pointer)))
+      (when ttf-font-struct (close-font ttf-font-struct))))
   (ttf-quit))
 
 (defun open-font (path-to-font point-size)
   "Open a font specified by the path specifier path-to-font sized to integer point-size (based on 72DPI). Returns a ttf-font struct and null on errors"
-  (autocollect (ptr)
-      (check-null (ttf-open-font (namestring path-to-font) point-size))
-    (ttf-close-font ptr)))
+  (let ((font (autocollect (ptr)
+                           (check-null (ttf-open-font (namestring path-to-font) point-size))
+                (ttf-close-font ptr))))
+    (push (tg:make-weak-pointer font) *fonts*)
+    font))
 
 (defun close-font (ttf-font-struct)
   "Frees the memory used by the ttf-font-struct"
   (tg:cancel-finalization ttf-font-struct)
+  (setf *fonts*
+        (remove ttf-font-struct *fonts*
+                :key #'tg:weak-pointer-value
+                :test #'(lambda (l r)
+                          (cffi:pointer-eq (autowrap:ptr l) (autowrap:ptr r)))))
   (ttf-close-font ttf-font-struct)
   (autowrap:invalidate ttf-font-struct))
