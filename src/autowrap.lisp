e61bb2119003d8ae7792d38aa11f7728d3ee5a00
Shawn Ellis <ellis@cachaca.local>
2021-12-18 13:31:26 -0600

    Allow multiple external includes when c2ffi is invoked

--- src/autowrap.lisp
+++ src/autowrap.lisp
@@ -5,8 +5,7 @@
                     :spec-path '(sdl2-ttf autowrap-spec)
                     :exclude-sources ("/usr/include/")
                     :include-sources ("SDL_ttf.h")
-		    :sysincludes `,(cl:append
-				    (cl:list (uiop:getenv "EXTRA_INCLUDES")))
+		    :sysincludes `,(uiop:split-string (uiop:getenv "EXTRA_INCLUDES") :separator " ")
                     :symbol-exceptions (("SDL_RWops" . "SDL-RWOPS"))
                     :exclude-constants ("^+__")
 		    :exclude-definitions ("_inline$"
